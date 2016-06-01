f49a49ee1a3bbed0efcd7af25fcf78a038683a30
sina <sina@sina.com>
2016-06-01 15:30:58 +0800

    demo Test

---
+++ test001/test001/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test001
+//
+//  Created by sina on 16/6/1.
+//  Copyright © 2016年 游梦创想. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test001/test001Tests/test001Tests.m
@@ -0,0 +1,39 @@
+//
+//  test001Tests.m
+//  test001Tests
+//
+//  Created by sina on 16/6/1.
+//  Copyright © 2016年 游梦创想. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface test001Tests : XCTestCase
+
+@end
+
+@implementation test001Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
